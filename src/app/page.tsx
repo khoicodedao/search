e4d76b5653546af43cd3efff198ffcb2efacc292
Khoi <dangkhoi29mta@gmail.com>
2023-10-31 11:30:35 +0700

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
-
+"use client";
+import { Card, Col, Row } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+import React from "react";
+import { Input, Space } from "antd";
+const { Search } = Input;
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: "#1677ff",
+    }}
+  />
+);
 export default function Home() {
+  const onSearch = (value: any, _e: any, info: any) =>
+    console.log(info?.source, value);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is home page
+    <main className="flex  flex-col items-center justify-between">
+      <div style={{ width: "90%" }}>
+        <Search
+          placeholder="Tìm kiếm văn bản"
+          allowClear
+          enterButton="Search"
+          size="large"
+          suffix={suffix}
+          onSearch={onSearch}
+        />
+      </div>
     </main>
   );
 }
