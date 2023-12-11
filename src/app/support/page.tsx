ef713e61f7169cf894eff5f3b7087a2f771d870b
Nguyễn Khôi <nguyenkhoi@MacBook-Air-cua-Nguyen.local>
2023-12-11 23:07:07 +0700

    [Add] Ai new

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,19 +1,37 @@
-// pages/support.js
-import React from "react";
+"use client";
 
-const Support = () => {
+import { useCompletion, useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
   return (
-    <div
-      style={{ height: "80vh" }}
-      className="flex flex-col  items-center justify-center  mr-5 ml-5"
-    >
-      <iframe
-        width={"100%"}
-        height={"100%"}
-        src="https://phimtienhiep.com/help/"
-      ></iframe>
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch text-black mb-24 pl-2 pr-2">
+      {messages.length > 0
+        ? messages.map((m) => (
+            <div key={m.id} className="whitespace-pre-wrap">
+              {m.role === "user" ? (
+                <img
+                  style={{ width: "35px", height: "35px" }}
+                  src="/logo.png"
+                />
+              ) : (
+                <img style={{ width: "28px", height: "28px" }} src="/AI.png" />
+              )}
+              <div className="rounded-lg bg-white p-4 m-2">{m.content}</div>
+            </div>
+          ))
+        : null}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          style={{ right: "0px" }}
+          className="fixed bottom-0 mb-24   w-full max-w-md p-2 border border-gray-300 rounded shadow-xl text-black"
+          value={input}
+          placeholder="Viết câu hỏi của bạn..."
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
-};
-
-export default Support;
+}
