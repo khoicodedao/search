529713417d2d0b9e4a91ec7bc540112b0c0a27a2
Khoi <dangkhoi29mta@gmail.com>
2023-12-11 10:21:14 +0700

    [Change] ngrok

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 export const GET = async (req: Request, res: Response) => {
-  const url = "http://10.32.119.98:4000/searchAll"; // Replace with your API URL
+  const url = process.env.NGROK_LINK + "searchAll"; // Replace with your API URL
 
   try {
     const response = await axios.get(url);
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 export const POST = async (req: Request, res: Response) => {
   const { query } = await req.json();
-  const url = "http://10.32.119.98:4000/search"; // Replace with your API URL
+  const url = process.env.NGROK_LINK + "search"; // Replace with your API URL
   // Define the request data as an object
   const requestData = {
     query,
