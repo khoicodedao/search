665dbdf0c484dae9171456b8d25b5cf02059aaad
Khoi <dangkhoi29mta@gmail.com>
2023-12-12 08:35:01 +0700

    [change key]

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 export const GET = async (req: Request, res: Response) => {
-  const url = process.env.NGROK_LINK + "searchAll"; // Replace with your API URL
+  const url = process.env.NGROK_LINK + "/searchAll"; // Replace with your API URL
 
   try {
     const response = await axios.get(url);
